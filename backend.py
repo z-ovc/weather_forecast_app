9e76f65d2991b5367cb01dbe605bfcd064b348cb
Zahra Oveisi <z.oveisi@ebcom.ir>
2023-06-27 07:57:50 +0330

    filter data needed from api

--- backend.py
+++ backend.py
@@ -6,9 +6,17 @@ def get_data(lat, lon, forecast_days = None, kind = None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api_key}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    nr_values = 8*forecast_days
+    filtered_data = filtered_data[:nr_values]
+    if kind =="temprature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if kind == "sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
 
 
 
 if __name__ == "__main__":
-    print(get_data(12.0282,12.0842323))
+    data = get_data(12.0282,12.0842323,forecast_days = 3, kind = "sky")
+    print(data)
--- ui.py
+++ ui.py
@@ -1,27 +1,29 @@
 import streamlit as st
 import plotly.express as px
-
+from backend import get_data
 
 st.title("weather forecast for next days".capitalize())
 
-place = st.text_input("place: ")
+lat, lon = st.text_input("place: lat: "),st.text_input("place: lon: ")
 
 days = st.slider("Forecast days", min_value=1, max_value=5, help="Select number for days to forecast")
 
 
 
 option = st.selectbox("select data to view",("temprature","sky"))
-st.subheader(f"{option} for next {days} in {place}")
+st.subheader(f"{option} for next {days} in place of {lat},{lon}")
 
-def get_data(days):
+# def get_data(days):
 
-    dates = ["2022-09-22","2022-09-23","2022-09-24"]
-    temprature = [10,11,15]
-    temprature = [days*i for i in temprature]
-    return dates,temprature
+#     dates = ["2022-09-22","2022-09-23","2022-09-24"]
+#     temprature = [10,11,15]
+#     temprature = [days*i for i in temprature]
+#     return dates,temprature
+# dates, temprature = get_data(days)
 
-dates, temprature = get_data(days)
+
+data = get_data(lat,lon,days,option)
+
 figure = px.line(x=dates, y=temprature , labels={"x":"Date","y":"temprature"})
 st.plotly_chart(figure)
 
-print(dates)
